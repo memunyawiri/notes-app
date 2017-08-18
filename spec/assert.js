b878f80ffed17d60d754bceadf04ad07e677ace9
Mutsa <mutsa.evelyn@hotmail.com>
2017-08-18 07:48:16 +0100

    Updated assert file: added isEqualTo assertion & tested it.

--- spec/assert.js
+++ spec/assert.js
@@ -5,5 +5,18 @@ var assert = {
     }else{
       console.log("Test has passed.");
     }
+  },
+
+  isEqualTo: function(arg1, arg2) {
+    if (arg1 === arg2) {
+      console.log("Test has passed: " + arg1 + " is equal to " + arg2);
+    }else{
+      throw new Error("Assertion failed: " + arg1 + " is not equal to " + arg2);
+    }
   }
-}
+};
+
+// function testEquality() {
+//   assert.isEqualTo(1,1);
+// }
+// testEquality();
