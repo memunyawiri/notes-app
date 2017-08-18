fc37c1ec384d9d65bf71ba179c0aa0ce292480f8
Mutsa <mutsa.evelyn@hotmail.com>
2017-08-18 08:15:03 +0100

    Spiked code that loads note: "Favourite drink: seltzer" onto index.html.

--- note-controller.js
+++ note-controller.js
@@ -1,8 +1,19 @@
 (function(exports) {
-  function changeText(newText) {
-    var elem = document.getElementById('app');
-    elem.innerHTML = newText;
+  function NoteController(noteList) {
+    this.noteList = noteList;
+    this.noteListView = new NoteListView(this.noteList);
+
   }
 
-  changeText('howdy');
+  NoteController.prototype.getsHtml = function () {
+    var htmlView = this.noteListView.viewHTML();
+    document.getElementById('app').innerHTML = htmlView;
+  };
+
+  exports.NoteController = NoteController;
 })(this);
+
+var noteList = new NoteList;
+noteList.createNote("Favourite drink: seltzer");
+var noteController = new NoteController(noteList);
+noteController.getsHtml();
